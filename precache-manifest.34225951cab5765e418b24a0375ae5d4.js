ec0f923bd575a49b5bb3c8eac5de235c78b94eb0
kgbhaumik86 <kgbhaumik86@gmail.com>
2020-07-17 15:48:54 +0530

    Updates

---
+++ precache-manifest.34225951cab5765e418b24a0375ae5d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07261900d595452813d6c840bb772b00",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "31856d8c8a3e4e99cf11",
+    "url": "/portfolio1/static/css/main.84c12adc.chunk.css"
+  },
+  {
+    "revision": "a32b2b6720c2f40def6e",
+    "url": "/portfolio1/static/js/2.447a0988.chunk.js"
+  },
+  {
+    "revision": "c2fedfbff5d199db39e48f93c8d15059",
+    "url": "/portfolio1/static/js/2.447a0988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31856d8c8a3e4e99cf11",
+    "url": "/portfolio1/static/js/main.b5900efd.chunk.js"
+  },
+  {
+    "revision": "c46925c61bb32700e33a",
+    "url": "/portfolio1/static/js/runtime-main.cbebb343.js"
+  },
+  {
+    "revision": "16c194944e694f60ee8e2dad5f936ead",
+    "url": "/portfolio1/static/media/kgb.16c19494.jpg"
+  }
+]);
